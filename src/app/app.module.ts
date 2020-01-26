02c2351d8d133193e89e3ce015d3fecf896df713
Danny Di Iorio <dannydiiorio@gmail.com>
2020-01-26 15:08:09 -0800

    add dark mode styling and toggle switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatTableModule, MatSnackBarModule, MatDividerModule,
+import { MatButtonModule, MatCardModule, MatTableModule, MatSnackBarModule, MatDividerModule, MatSlideToggleModule,
   MatToolbarModule, MatMenuModule, MatIconModule, MatRippleModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NotificationsService } from './services/notifications.service';
 import { ResumeComponent } from './resume/resume.component';
 
@@ -21,7 +20,6 @@ import { ResumeComponent } from './resume/resume.component';
     NavMenuComponent,
     HomeComponent,
     PortfolioComponent,
-    FetchDataComponent,
     ResumeComponent
   ],
   imports: [
@@ -39,11 +37,11 @@ import { ResumeComponent } from './resume/resume.component';
     MatIconModule, 
     MatRippleModule,
     MatListModule,
+    MatSlideToggleModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, data: {animation: 'Home'}, pathMatch: 'full' },
       { path: 'portfolio', component: PortfolioComponent, data: {animation: 'Portfolio'} },
-      { path: 'resume', component: ResumeComponent, data: {animation: 'Resume'} },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'resume', component: ResumeComponent, data: {animation: 'Resume'} }
     ])
   ],
   providers: [
--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -6,6 +6,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./nav-menu.component.less']
 })
 export class NavMenuComponent {
+  currentTheme: string
   @ViewChild('navbarToggler') navbarToggler:ElementRef;
 
   navBarTogglerIsVisible() {
@@ -15,6 +16,34 @@ export class NavMenuComponent {
   public collapseNav() {
     if (this.navBarTogglerIsVisible()) {
       this.navbarToggler.nativeElement.click();
+    }
+  }
+
+  public switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    }
+    else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }    
+  }
+
+  ngOnInit() {
+    this.currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+  }
+
+  ngAfterViewInit() {
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    toggleSwitch.addEventListener('change', this.switchTheme, false);
+
+    if (this.currentTheme) {
+      document.documentElement.setAttribute('data-theme', this.currentTheme);
+
+      if (this.currentTheme === 'dark') {
+          toggleSwitch.checked = true;
+      }
     }
   }
 }
